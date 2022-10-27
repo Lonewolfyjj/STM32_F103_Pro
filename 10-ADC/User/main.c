d62b491ce5d486243ff3d7e0f7ea3165cfd44c7d
UserNamezhangxi <zhangxi_my@sina.cn>
2022-10-28 00:37:31 +0800

    ADC 数据采样，使用连续转换非扫描模式，采集数据串口发送给电脑

---
+++ 10-ADC/User/main.c
@@ -0,0 +1,69 @@
+#include "reg.h"
+#include "key.h"
+#include "delay.h"
+#include "led.h"
+#include "beep.h"
+#include "typedef.h"
+#include "string.h"
+#include "usart.h"
+#include "timer.h"
+#include "adc.h"
+
+void Rcc_init(void);
+void key_test(void);
+extern u8  TIM5CH1_CAPTURE_STA;		//输入捕获状态		    				
+extern u16	TIM5CH1_CAPTURE_VAL;	//输入捕获值
+int main()
+{
+
+	u16 adcResult;
+	float temp=0;
+	Rcc_init();
+	delay_init();
+  usart_init(72,115200);
+  init_adc();
+	while(1)							
+	{
+		adcResult = Get_Adc_Average();
+		temp = adcResult*(3.3/4096);
+		printf("ADC result = %f\r\n", temp);	//IC_TIM3_CH1_GetDuty() 
+	}
+}
+
+
+void Rcc_init(void) 
+{
+	// 1.RCC 寄存器打开内部时钟
+	RCC1->CR |= 1<<0;
+	// 2. 关闭RCC_CIR 中断，避免中断干扰时钟设置
+  RCC1->CIR = 0;
+  // 3. 设置HSE 的信号源外部晶振不旁路，启动HSE 	
+	RCC1->CR &= ~(1<<18);
+	// 4. 开启HSE
+	RCC1->CR |= 1<<16;
+	// 5. 等待HSE 就绪
+	while((RCC->CR & (1<<17)) == 0);
+	// 6. 配置 AHP APB1 APB2分频倍频系数
+	RCC1->CFGR &= ~(1<<7); // AHB=DIV1;   
+	RCC1->CFGR |= 4<<8;    // APB1=DIV2 
+	RCC1->CFGR &= ~(1<<13); // APB2=DIV1;
+	// 7. 关闭PLL
+	RCC1->CR &= ~(1<<24);  // 关闭PLL
+	// 8. 配置PLL 分频倍频
+	RCC1->CFGR |= 0xf<<18; // 16 倍频
+	RCC1->CFGR |= 1<<16; // PLLSRC时钟源选择
+	RCC1->CFGR |= 1<<17; //PLLXTPRE 分频选择
+  FLASH_ACR |=0x32; //FLASH 2个延时周期
+  // 9. 使能PLL
+	RCC1->CR |= (1<<24); // 开启PLL
+	// 10. 等待PLL 使能OK
+	while((RCC1->CR & (1<<25)) == 0); 
+	// 11. 选择主PLL 作为时钟
+	RCC1->CFGR &= ~(3<<0); // 清零
+	RCC1->CFGR |= 2<<0;
+	// 12.等待主PLL稳定
+	while((RCC1->CFGR & (3<<2)) != (2<<2));	
+
+}
+
+
